2541871d8ec49091204885d2c3f6afc9c6309f88
Arvin <Arvinwijsman@gmail.com>
2019-06-28 19:51:30 +0200

    Change password tests

--- wagtail/asv_tests/unit/test_change_password.py
+++ wagtail/asv_tests/unit/test_change_password.py
@@ -22,11 +22,12 @@ class TestChangePassword(BaseTest):
         self.mock_form = MagicMock()
         self.mock_form.is_valid = MagicMock(return_value=True)
 
-    @mock.patch('wagtail.admin.views.account.password_management_enabled', return_value=True, autospec=True)
+    @mock.patch('wagtail.admin.views.account.password_management_enabled', autospec=True)
     @mock.patch('django.contrib.auth.forms.PasswordChangeForm', autospec=True)
     @mock.patch('django.contrib.auth.update_session_auth_hash', autospec=True)
     def test_change_password(self, mocked_pme, mocked_password_change_form, mocked_update_session):
         # Arrange
+        mocked_pme.return_value = True
         mocked_password_change_form.return_value = self.mock_form
 
         # Act
@@ -35,6 +36,22 @@ class TestChangePassword(BaseTest):
         # Assert
         assert mocked_update_session.called
         assert response.status_code == 302 and response.url == '/admin/account/'
+
+    @mock.patch('wagtail.admin.views.account.password_management_enabled', autospec=True)
+    @mock.patch('django.contrib.auth.forms.PasswordChangeForm', autospec=True)
+    def test_get_password_form(self, mocked_pme, mocked_password_change_form):
+        # Arrange
+        self.mock_request.method = 'GET'
+        self.mock_request.user.has_usable_password = MagicMock(return_value=True)
+        mocked_pme.return_value = True
+        mocked_password_change_form.return_value = self.mock_form
+
+        with patch('django.template.loader.render_to_string') as mock_renderer:
+            # Act
+            response = change_password(self.mock_request)
+        
+        # Assert
+        assert mocked_password_change_form.called and mock_renderer.called
 
     @mock.patch('wagtail.admin.views.account.password_management_enabled', autospec=True)
     def test_management_disabled(self, mocked_password_management):
